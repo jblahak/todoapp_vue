726e23ee27aca09ac69b452c376afac6dc8c496f
sinouJ <sinoujordan@gmail.com>
2021-03-16 21:06:05 +0100

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand href="#">TodoApp</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <router-link to="/">Home</router-link>
+          </b-navbar-nav>
+
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown right>
+              <template #button-content>
+                <em>Hi</em>
+              </template>
+              <router-link class="dropdown-item" to="/login">Login</router-link>
+              <b-dropdown-item href="#">Sign up</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -21,12 +40,18 @@
   padding: 30px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+nav {
+  .navbar-nav {
+    a {
+      color: white;
+    }
+
+    .dropdown {
+      a {
+        color: #2c3e50;
+      }
+    }
+  }
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
