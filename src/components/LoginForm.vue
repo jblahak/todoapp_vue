679fbd4d66e0e2e4a2b9dc232b37bb301d0ed125
sinouJ <sinoujordan@gmail.com>
2021-03-16 22:42:26 +0100

    login view

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <b-form class="col-8">
+    <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+    >
+        <b-form-input
+            id="input-1"
+            v-model="mutableEmail"
+            type="email"
+            placeholder="Enter email"
+            required
+        ></b-form-input>
+    </b-form-group>
+    <b-form-group id="input-group-2" label="Your Password:" label-for="input-2">
+        <b-form-input
+            id="input-2"
+            v-model="mutablePassword"
+            placeholder="Enter password"
+            required
+        ></b-form-input>
+    </b-form-group>
+    <b-button type="submit" variant="success">Login</b-button>
+  </b-form>
+</template>
+
+<script>
+export default {
+    name: 'LoginForm',
+    props: {
+        email: String,
+        password: String
+    },
+    data() {
+        return {
+            mutableEmail: this.email,
+            mutablePassword: this.password
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="d-flex align-items-center justify-content-center flex-column">
+    <h1>Login</h1>
+    <login-form
+      :password="form.password"
+      :email="form.email"
+    />
+  </div>
+</template>
+
+<script>
+import LoginForm from '../components/LoginForm'
+
+export default {
+    data() {
+      return {
+        form: {
+          password: '',
+          email: ''
+        }
+      }
+    },
+    name: 'Login',
+    components: {
+      LoginForm
+    }
+}
+</script>
+
+<style>
+
+</style>
