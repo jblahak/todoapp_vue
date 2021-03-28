02821fec5ef83906ff5f3f499ee2a605fb199538
sinouJ <sinoujordan@gmail.com>
2021-03-28 03:09:26 +0200

    profile vue

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,10 +1,18 @@
 <template>
-  <div v-if="user && token && token.isLogged">
-      Hi, {{user.username}}
-  </div>
-  <div v-else>
-      Oops
-  </div>
+    <div v-if="user && token && token.isLogged">
+        <b-jumbotron>
+            <template #header>Hi, {{user.username}}</template>
+            <template #lead>
+                <b>Biography</b><br>
+                {{user.bio}}
+            </template>
+            <hr class="my-4">
+            <p>Email : {{user.email}}</p>
+            <hr class="my-4">
+            <p>Here since : {{createdDate}}</p>
+            <b-button variant="primary" href="#">Edit my profile</b-button>
+        </b-jumbotron>
+    </div>
 </template>
 
 <script>
@@ -12,6 +20,9 @@ import Cookies from 'js-cookie'
 import jwt from 'jsonwebtoken'
 import { mapGetters } from 'vuex'
 import users from '../api/users'
+import dayjs from 'dayjs'
+
+import locale from 'dayjs/locale/fr'
 
 export default {
     name: 'Profile',
@@ -26,6 +37,12 @@ export default {
                     const user = users.getUser(this.token.token)
                     return user
                 }
+            }
+        },
+        createdDate: function() {
+            if (this.user) {
+                return dayjs(this.user.createdAt).locale('fr').format('D MMMM YYYY')
+                console.log(this.user)
             }
         }
     },
